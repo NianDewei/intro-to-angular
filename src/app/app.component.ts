daa9dc3ecb08d5973424906fe829a85b51cff62f
Gilver Rolando Ordoñez <admi.jmb@gmail.com>
2022-05-16 18:01:09 -0500

    01 | Accumulator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'intro-to-angular';
+  title: string = 'Dev Angular';
+  number: number = 1;
+
+  // // add +1 to counter
+  // add(): void {
+  //   this.number++;
+  // }
+
+  // // subtract -1 to counter
+  // subtract(): void {
+  //   if (this.number > 0) this.number--;
+  // }
+
+  // acumulator counter
+  acumulator(value: number): void {
+    if(this.number == 1 && value == -1) this.number = 1;
+    else this.number += value;
+  }
 }
